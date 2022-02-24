16524636b7cc161f108138bd07146a16ee1ec8e5
Kalos99 <calogerocorsello1999@gmail.com>
2022-02-24 21:50:47 +0100

    Scheletro TestGestioneCurricula

--- src/it/gestionecurricula/test/TestGestioneCurricula.java
+++ src/it/gestionecurricula/test/TestGestioneCurricula.java
@@ -1,7 +1,53 @@
 package it.gestionecurricula.test;
 
+import it.gestionecurricula.service.MyServiceFactory;
+import it.gestionecurricula.service.curriculum.CurriculumService;
+import it.gestionecurricula.service.esperienza.EsperienzaService;
+
 public class TestGestioneCurricula {
 	public static void main(String[] args) {
+		
+		// parlo direttamente con il service
+		CurriculumService curriculumService = MyServiceFactory.getCurriculumServiceImpl();
+		EsperienzaService esperienzaService = MyServiceFactory.getEsperienzaServiceImpl();
+
+		try {
+
+			// ora con il service posso fare tutte le invocazioni che mi servono
+			System.out.println("In tabella ci sono " + curriculumService.listAll().size() + " elementi.");
+			
+			System.out.println("In tabella ci sono " + esperienzaService.listAll().size() + " elementi.");
+
+			testInserimentoNuovoCurriculum(curriculumService);
+			System.out.println("In tabella ci sono " + curriculumService.listAll().size() + " elementi.");
+			
+			testInserimentoNuovaEsperienza(esperienzaService);
+			System.out.println("In tabella ci sono " + esperienzaService.listAll().size() + " elementi.");
+
+			testRimozioneCurriculum(curriculumService);
+			System.out.println("In tabella ci sono " + curriculumService.listAll().size() + " elementi.");
+			
+			testRimozioneEsperienza(esperienzaService);
+			System.out.println("In tabella ci sono " + esperienzaService.listAll().size() + " elementi.");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void testInserimentoNuovoCurriculum(CurriculumService curriculumService) throws Exception{
+		
+	}
+	
+	private static void testInserimentoNuovaEsperienza(EsperienzaService esperienzaService) throws Exception{
+		
+	}
+	
+	private static void testRimozioneCurriculum(CurriculumService curriculumService) throws Exception{
+		
+	}
+	
+	private static void testRimozioneEsperienza(EsperienzaService esperienzaService) throws Exception{
 		
 	}
 }
