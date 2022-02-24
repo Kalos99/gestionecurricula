beed64f988dc08427e014fb457b897299589f459
Kalos99 <calogerocorsello1999@gmail.com>
2022-02-24 21:20:13 +0100

    CurriculumServiceImpl e EsperienzaServiceImpl senza controlli

--- src/it/gestionecurricula/service/curriculum/CurriculumServiceImpl.java
+++ src/it/gestionecurricula/service/curriculum/CurriculumServiceImpl.java
@@ -1,52 +1,140 @@
 package it.gestionecurricula.service.curriculum;
 
+import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
 
+import it.gestionecurricula.connection.MyConnection;
+import it.gestionecurricula.dao.Constants;
 import it.gestionecurricula.dao.curriculum.CurriculumDAO;
 import it.gestionecurricula.model.Curriculum;
 
 public class CurriculumServiceImpl implements CurriculumService{
 
+	private CurriculumDAO curriculumDao;
 	@Override
 	public void setCurriculumDao(CurriculumDAO curriculumDao) {
-		// TODO Auto-generated method stub
-		
+		this.curriculumDao = curriculumDao;	
 	}
 
 	@Override
 	public List<Curriculum> listAll() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		List<Curriculum> result = new ArrayList<>();
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			curriculumDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = curriculumDao.list();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public Curriculum findById(Long idInput) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (idInput == null || idInput < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		Curriculum result = null;
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			curriculumDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = curriculumDao.get(idInput);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int aggiorna(Curriculum input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (input == null || input.getId() == null || input.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			curriculumDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = curriculumDao.update(input);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int inserisciNuovo(Curriculum input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (input == null)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			curriculumDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = curriculumDao.insert(input);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int rimuovi(Curriculum input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (input == null || input.getId() == null || input.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			curriculumDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = curriculumDao.delete(input);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public List<Curriculum> findByExample(Curriculum input) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		List<Curriculum> result = new ArrayList<>();
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			curriculumDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = curriculumDao.findByExample(input);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 }
--- src/it/gestionecurricula/service/esperienza/EsperienzaServiceImpl.java
+++ src/it/gestionecurricula/service/esperienza/EsperienzaServiceImpl.java
@@ -1,52 +1,142 @@
 package it.gestionecurricula.service.esperienza;
 
+import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
 
+import it.gestionecurricula.connection.MyConnection;
+import it.gestionecurricula.dao.Constants;
 import it.gestionecurricula.dao.esperienza.EsperienzaDAO;
 import it.gestionecurricula.model.Esperienza;
 
 public class EsperienzaServiceImpl implements EsperienzaService {
-
+	
+	private EsperienzaDAO esperienzaDao;
+	
 	@Override
 	public void setEsperienzaDao(EsperienzaDAO esperienzaDao) {
-		// TODO Auto-generated method stub
-		
+		this.esperienzaDao = esperienzaDao;
 	}
+
 
 	@Override
 	public List<Esperienza> listAll() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		List<Esperienza> result = new ArrayList<>();
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			esperienzaDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = esperienzaDao.list();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public Esperienza findById(Long idInput) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (idInput == null || idInput < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		Esperienza result = null;
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			esperienzaDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = esperienzaDao.get(idInput);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int aggiorna(Esperienza input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (input == null || input.getId() == null || input.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			esperienzaDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = esperienzaDao.update(input);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int inserisciNuovo(Esperienza input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (input == null)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			esperienzaDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = esperienzaDao.insert(input);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public int rimuovi(Esperienza input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (input == null || input.getId() == null || input.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			esperienzaDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = esperienzaDao.delete(input);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 	@Override
 	public List<Esperienza> findByExample(Esperienza input) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		List<Esperienza> result = new ArrayList<>();
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+
+			// inietto la connection nel dao
+			esperienzaDao.setConnection(connection);
+
+			// eseguo quello che realmente devo fare
+			result = esperienzaDao.findByExample(input);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
 	}
 
 }
